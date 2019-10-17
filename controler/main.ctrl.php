1510c286009c29670e390a3401da1e5c6c84fefc
GALERA Theo <to.38@live.fr>
2019-10-17 09:52:03 +0200

    debut main.ctrl.php

---
+++ controler/main.ctrl.php
@@ -0,0 +1,23 @@
+//Le controleur main doit fournir à la vue main :
+  - Les catégories et sous catégoriess
+  - Les articles mis en vedette sur la page principale
+<?php
+// Inclusion du framework
+include_once("../framework/view.class.php");
+
+// Inclusion du modèle
+include_once("../model/DAO.class.php");
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
