1e141138c8c8e9cd828286411aeb67febc34f7e6
ByLycheeZ <theo_p@orange.fr>
2019-10-20 19:16:26 +0200

    Design page d'accueil amélioré

--- view/main.view.php
+++ view/main.view.php
@@ -18,34 +18,59 @@
             </div>
             <div class="cats">
                 <select>
-                    <option value="categories" selected>Toutes les catégories</option>
+                    <option value="toutes" selected>Toutes les catégories</option>
                     <option value="test1">Test1</option>
                     <option value="test2">Test2</option>
                     <option value="test3">Test3</option>
+                    <option value="test4">Test4</option>
+                    <option value="test5">Test5</option>
+                    <option value="test6">Test6</option>
+                    <option value="test7">Test7</option>
+                    <option value="test8">Test8</option>
+                    <option value="test9">Test9</option>
+                    <option value="test10">Test10</option>
+                    <option value="test11">Test11</option>
+                    <option value="test12">Test12</option>
+                    <option value="test13">Test13</option>
                 </select>
             </div>
             <div class="midBarre">
                 <form action="index.html" method="post">
                     <input id="barreRecherche" type="text" placeholder="Que recherchez-vous ?">
                 </form>
-                <a href="../recherche.ctrl.php"> <img id="loupe" src="../view/design/loupe.png" alt="loupe"> </a>
+                <a href="../controler/recherche.ctrl.php"> <img id="loupe" src="../view/design/loupe.png" alt="loupe"> </a>
             </div>
             <div class="pan">
                 <a href="../controler/panier.ctrl.php"> <img id="panier" src="../view/design/panier.png" alt="panier"> </a>
             </div>
-            <div class="rigthBar">
-                <a class="signLog" href="../controler/inscription.ctrl.php">S'inscrire</a>
-                <a class="signLog" href="../controler/connexion.ctrl.php">Se connecter</a>
+            <div class="rightBar">
+                <ul>
+                    <li> <a class="signLog" href="../controler/connexion.ctrl.php">Se connecter</a> </li>
+                    <li> <a class="signLog" href="../controler/inscription.ctrl.php">S'inscrire</a> </li>
+                </ul>
             </div>
         </header>
-        <div class="zone">
+        <div class="catLat">
             <nav>
                 <ul>
-                    <li>Test1</li>
-                    <li>Test2</li>
-                    <li>Test3</li>
+                    <li> <br> Test1</li>
+                    <li> <br> Test2</li>
+                    <li> <br> Test3</li>
+                    <li> <br> Test4</li>
+                    <li> <br> Test5</li>
+                    <li> <br> Test6</li>
+                    <li> <br> Test7</li>
+                    <li> <br> Test8</li>
+                    <li> <br> Test9</li>
+                    <li> <br> Test10</li>
+                    <li> <br> Test11</li>
+                    <li> <br> Test12</li>
+                    <li> <br> Test13</li>
                 </ul>
             </nav>
+        </div>
+        <div class="zone">
+            <a href="#"> <img id="banniere_noel" src="../view/design/banniere_noel.jpg" alt="bannière noël"> </a>
         </div>
     </body>
 </html>
