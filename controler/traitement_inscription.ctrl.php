3f87b11be4c3ab9dc63903b02dc6384af58502c7
PONCHON Theo <ponchoth@hostname.iut2.upmf-grenoble.fr>
2019-10-24 09:14:47 +0200

    Inscription operationnelle

--- controler/traitement_inscription.ctrl.php
+++ controler/traitement_inscription.ctrl.php
@@ -6,31 +6,13 @@
     include_once("../model/DAO.class.php");
 
     $vue = new View();
-    
+
     if ($_POST['pass'] != $_POST['confirm']) {
         $vue->assign('message', "Les mots de passe ne concordent pas !");
         $vue->display('../view/inscription.view.php');
     } else {
         $dao = new DAO();
-        $sql = "INSERT INTO Client (email, nom, prenom, mdp, adresse, tel)
-                values (:email, :nom, :prenom, :mdp, :adresse, :tel)";
-        $stmt = $dao->prepare($sql);
-
-        $email = $_POST['mail'];
-        $nom = $_POST['nom'];
-        $prenom = $_POST['prenom'];
-        $mdp = $_POST['pass'];
-        $adresse = $_POST['adresse'];
-        $tel = $_POST['tel'];
-
-        $stmt->BindParam(':email', $email);
-        $stmt->BindParam(':nom', $nom);
-        $stmt->BindParam(':prenom', $prenom);
-        $stmt->BindParam(':mdp', $mdp);
-        $stmt->BindParam(':adresse', $adresse);
-        $stmt->BindParam(':tel', $tel);
-
-        $stmt->execute();
+        $dao->inscrire($_POST['mail'], $_POST['nom'], $_POST['prenom'],
+                        $_POST['pass'], $_POST['adresse'], $_POST['tel']);
     }
-
 ?>
